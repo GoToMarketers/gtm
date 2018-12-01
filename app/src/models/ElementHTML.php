b983230102cab4ee75d489c21ba6dd74b9c6cb2a
richardrudy <rick@gotomarketers.com>
2018-12-01 15:24:15 -0500

    enabe HTML ELements

--- app/src/models/ElementHTML.php
+++ app/src/models/ElementHTML.php
@@ -1,38 +1,38 @@
 <?php
 
-//namespace GoToMarketers\Models\Elements;
-//
-//use DNADesign\Elemental\Models\ElementContent;
-//use NathanCox\CodeEditorField\CodeEditorField;
-//
-//class ElementHTML extends ElementContent {
-//
-//    private static $singular_name = 'HTML Code Element';
-//
-//    private static $plural_name = 'HTML Code Elements';
-//
-//    private static $description = 'Renders arbitrary HTML Code in an Element ';
-//
-//    private static $table_name = 'ElementHTML';
-//
-//    public function getCMSFields()
-//    {
-//        $fields = parent::getCMSFields();
-//
-//        $fields->replaceField(
-//            'HTML',
-//            CodeEditorField::create('HTML', 'HTML Code')
-//                           ->setRows(30)
-//                           ->setMode('html')
-//                           ->setTheme('twilight')
-//        );
-//
-//        return $fields;
-//    }
-//
-//    public function getType()
-//    {
-//        return 'HTML Element';
-//    }
-//
-//}
+namespace GoToMarketers\Models\Elements;
+
+use DNADesign\Elemental\Models\ElementContent;
+use NathanCox\CodeEditorField\CodeEditorField;
+
+class ElementHTML extends ElementContent {
+
+    private static $singular_name = 'HTML Code Element';
+
+    private static $plural_name = 'HTML Code Elements';
+
+    private static $description = 'Renders arbitrary HTML Code in an Element ';
+
+    private static $table_name = 'ElementHTML';
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->replaceField(
+            'HTML',
+            CodeEditorField::create('HTML', 'HTML Code')
+                           ->setRows(30)
+                           ->setMode('html')
+                           ->setTheme('twilight')
+        );
+
+        return $fields;
+    }
+
+    public function getType()
+    {
+        return 'HTML Element';
+    }
+
+}
--- app/src/models/ElementRawHTML.php
+++ app/src/models/ElementRawHTML.php
@@ -1,44 +1,44 @@
 <?php
 
-//namespace GoToMarketers\Models\Elements;
-//
-//use DNADesign\Elemental\Models\ElementContent;
-//use NathanCox\CodeEditorField\CodeEditorField;
-//
-//class ElementRawHTML extends ElementContent {
-//
-//    private static $singular_name = 'RAW HTML Code';
-//
-//    private static $plural_name = 'RAW HTML Code';
-//
-//    private static $description = 'Renders arbitrary HTML Code';
-//
-//    private static $controller_template = 'NakedElement';
-//
-//    private static $table_name = 'ElementRawHTML';
-//
-//    public function getCMSFields()
-//    {
-//        $fields = parent::getCMSFields();
-//
-//        $fields->replaceField(
-//            'HTML',
-//            CodeEditorField::create('HTML', 'HTML Code')
-//                           ->setRows(30)
-//                           ->setMode('html')
-//                           ->setTheme('twilight')
-//        );
-//
-//        $fields->removeFieldFromTab('Root.Main', 'TitleTag');
-//        $fields->removeFieldFromTab('Root.Main', 'TitleClass');
-//        $fields->removeFieldFromTab('Root.Main', 'BackgroundImage');
-//
-//        return $fields;
-//    }
-//
-//    public function getType()
-//    {
-//        return 'Raw HTML';
-//    }
-//
-//}
+namespace GoToMarketers\Models\Elements;
+
+use DNADesign\Elemental\Models\ElementContent;
+use NathanCox\CodeEditorField\CodeEditorField;
+
+class ElementRawHTML extends ElementContent {
+
+    private static $singular_name = 'RAW HTML Code';
+
+    private static $plural_name = 'RAW HTML Code';
+
+    private static $description = 'Renders arbitrary HTML Code';
+
+    private static $controller_template = 'NakedElement';
+
+    private static $table_name = 'ElementRawHTML';
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->replaceField(
+            'HTML',
+            CodeEditorField::create('HTML', 'HTML Code')
+                           ->setRows(30)
+                           ->setMode('html')
+                           ->setTheme('twilight')
+        );
+
+        $fields->removeFieldFromTab('Root.Main', 'TitleTag');
+        $fields->removeFieldFromTab('Root.Main', 'TitleClass');
+        $fields->removeFieldFromTab('Root.Main', 'BackgroundImage');
+
+        return $fields;
+    }
+
+    public function getType()
+    {
+        return 'Raw HTML';
+    }
+
+}
